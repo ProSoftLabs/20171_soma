f3704d4a252d0b4e4511d9002f5a1e4f49a95c4d
Hugo Galvão <hugogalvao@MacBook-Air-de-Hugo.local>
2017-05-23 21:29:57 -0300

    Timeline grouped by days of week

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -5,6 +5,9 @@ class ListingController < ApplicationController
   def load_tweets
     @user_name = params[:user]
     @timeline = Client.user_timeline(@user_name, {:count => 100})
-    @timeline_hours = Tweet.count_tweets_by_time(@timeline)
+    timeline_hours = Tweet.count_tweets_by_hours(@timeline)
+    timeline_days = Tweet.count_tweets_by_days(@timeline)
+    @timeline_details = { :groupedByHour => timeline_hours, :groupedByDay =>
+      timeline_days }
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,26 +1,27 @@
 class Tweet
   class << self # change all class methods to static
 
-    def count_tweets_by_time(timeline)
+    def count_tweets_by_hours(timeline)
       hours = {}
       (0..23).each {|x| hours[x] = 0}
 
       timeline.each do |tweet|
-        hour = hour_regex(tweet.created_at.to_s).to_i
+        hour = tweet.created_at.strftime("%H").to_i
         hours[hour] += 1
       end
 
       hours
     end
 
-    private
+    def count_tweets_by_days(timeline)
+      days = { "Monday" => 0, "Tuesday" => 0, "Wednesday" => 0, "Thursday" => 0,
+        "Friday" => 0, "Saturday" => 0, "Sunday" => 0 }
+      timeline.each do |tweet|
+        day = tweet.created_at.strftime("%A").to_s
+        days[day] += 1
+      end
 
-    def hour_regex(date)
-      #2017-05-14 00:21:08 +0000
-      date = date.split[1]
-      date = date[0..1]
-
-      date
+      days
     end
   end
 end
