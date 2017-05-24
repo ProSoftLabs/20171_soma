a78da526d4deb247d1f032d60df4f3c8c2c04d9e
Kaio Max <kaiomaks@gmail.com>
2017-05-23 23:46:07 -0300

    Show twitter activity by hours and days

--- app/assets/javascripts/components/timeline.jsx
+++ app/assets/javascripts/components/timeline.jsx
@@ -1,82 +1,62 @@
 class Timeline extends React.Component {
   componentDidMount() {
-    const data = this.parseTimeline();
-    console.log('DATA', data);
-    const chartData = this.parseChartData(data);
-    console.log('chartData', chartData);
+    this.renderChart(
+      'chart-container-1',
+      'Total Tweets by Hour',
+      _.keys(this.props.timelineDetails.groupedByHour),
+      [
+        {
+          name: 'Tweets',
+          data: _.values(this.props.timelineDetails.groupedByHour)
+        }
+      ]
+    );
 
-    Highcharts.chart('chart-container', {
-        chart: {
-            type: 'bar'
-        },
+    this.renderChart(
+      'chart-container-2',
+      'Total Tweets by Day',
+      _.keys(this.props.timelineDetails.groupedByDay),
+      [
+        {
+          name: 'Tweets',
+          data: _.values(this.props.timelineDetails.groupedByDay)
+        }
+      ]
+    );
+  }
+
+  renderChart(container, title, categories, series) {
+    Highcharts.chart(container, {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: `${ title } - ${ this.props.userName }`
+      },
+      xAxis: {
+        categories: categories
+      },
+      yAxis: {
         title: {
-            text: `Last Tweets Analysis - ${this.props.userName}`
-        },
-        xAxis: {
-            categories: chartData.categories
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Total'
-            }
-        },
-        legend: {
-            reversed: true
-        },
-        plotOptions: {
-            series: {
-                stacking: 'normal'
-            }
-        },
-        series: chartData.series
+          text: 'Total'
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      series
     });
-  }
-
-  parseTimeline() {
-    const { timeline } = this.props;
-    const grouped = _.groupBy(timeline, (tweet) => {
-      return moment(tweet.created_at).format("DD/MM/YYYY");
-    });
-    console.log('grouped', grouped);
-
-    const parsedData = _.map(grouped, (tweets, day) => {
-      const totalFavorites = _.sumBy(tweets, 'favorite_count');
-      const totalRetweets = _.sumBy(tweets, 'retweet_count');
-      
-      return {
-        day,
-        originalDate: tweets.length > 0 && tweets[0].created_at,
-        totalFavorites,
-        totalRetweets
-      };
-    });
-    console.log('parsedData', parsedData);
-
-    return _.sortBy(parsedData, (dayData) => {
-      return moment(dayData.originalDate).toISOString();
-    });
-  }
-
-  parseChartData(data) {
-    return {
-      categories: _.map(data, (entry) => entry.day),
-      series: [{
-        name: 'Retweets Total',
-        data: _.map(data, (entry) => entry.totalRetweets)
-      }, {
-        name: 'Favorites Total',
-        data: _.map(data, (entry) => entry.totalFavorites)
-      }]
-    };
   }
 
   render() {
-    console.log(this.props.timeline);
-    this.parseTimeline();
     return (
-      <div>
-        <div id="chart-container" style={ { height: '600px' } }></div>
+      <div className="row">
+        <div className="col-7">
+          <div id="chart-container-1" style={ { height: '400px' } }></div>
+        </div>
+        <div className="col-5">
+          <div id="chart-container-2" style={ { height: '400px' } }></div>
+        </div>
       </div>
     );
   }
--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -4,10 +4,14 @@ class ListingController < ApplicationController
 
   def load_tweets
     @user_name = params[:user]
-    @timeline = Client.user_timeline(@user_name, {:count => 100})
-    timeline_hours = Tweet.count_tweets_by_hours(@timeline)
-    timeline_days = Tweet.count_tweets_by_days(@timeline)
-    @timeline_details = { :groupedByHour => timeline_hours, :groupedByDay =>
-      timeline_days }
+
+    timeline = Client.user_timeline(@user_name, {:count => 200})
+    timeline_hours = Tweet.count_tweets_by_hours(timeline)
+    timeline_days = Tweet.count_tweets_by_days(timeline)
+
+    @timeline_details = {
+      :groupedByHour => timeline_hours,
+      :groupedByDay => timeline_days
+    }
   end
 end
